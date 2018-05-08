676efa6e3fe319635244c05ec0168b42be7f512a
Matt Murphy <murphyma@grinnell.edu>
2018-05-08 08:20:37 -0500

    formatting

--- graph/node.h
+++ graph/node.h
@@ -13,18 +13,17 @@ extern "C" {
 
 struct graph_node;
 
-typedef struct list_node {
-    struct graph_node* graph_node;
-    struct list_node* next;
-} list_node_t;
-
 typedef struct graph_node {
-    char type; // S or C
-    const char* val;
-    list_node_t* neighbors;
-    pthread_mutex_t m;
+  char type; // S or C
+  const char* val;
+  list_node_t* neighbors;
+  pthread_mutex_t m;
 } graph_node_t;
 
+typedef struct list_node {
+  graph_node_t* graph_node;
+  struct list_node* next;
+} list_node_t;
 
 graph_node_t* add_node(char type, const char* val);
 
@@ -33,7 +32,7 @@ bool _compare_node(graph_node_t* node1, graph_node_t* node2);
 void add_neighbor(graph_node_t* node1, graph_node_t* node2);
 
 void delete_node(graph_node_t* sad_node);
-
+//
 // This makes the header file work for both C and C++
 #ifdef __cplusplus
 }
