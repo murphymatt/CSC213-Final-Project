b8354d346a1447cbb00625cbbc6d872e84c27dad
Henry Fisher <henry.wfisher@gmail.com>
2018-05-08 08:23:34 -0500

    Adding io so I can debug on a linux machine

--- io/io.c
+++ io/io.c
@@ -3,7 +3,7 @@
 /*
  * out_file is a E+1 line file where E is edges in graph
  * first line:  Type, Value; Type2, Value2; etc
- * subsequent: Type, Value: Type2, Value2; Type3, Value3 
+ * subsequent: Type, Value; Type2, Value2; Type3, Value3 
  *    where Value has an edge with Value2, and Value3
  */
 void write_to_file(hash_table_t *ht, const char *file_path) {
@@ -28,7 +28,7 @@ void write_to_file(hash_table_t *ht, const char *file_path) {
         while (current != NULL) {
             fprintf(fp, "\n");
             current->graph_node->flag = 1;
-            fprintf(fp, "%c, %s: ", current->graph_node->type, current->graph_node->val);
+            fprintf(fp, "%c, %s; ", current->graph_node->type, current->graph_node->val);
 
             list_node_t* cur_neighbor = current->graph_node->neighbors;
             while (cur_neighbor != NULL) {
@@ -59,11 +59,11 @@ hash_table_t* read_from_file(const char *file_path) {
         int ret = get_next_name(fp, name, &type);
         if (ret == '\n')
             break;
-
+        printf("%c, %s\n", type, name);
         graph_node_t* new_node = add_node(type, name);
         add(ht, new_node);
         // get rid of the next space
-        char c = fgetc(fp);
+        char nothing = fgetc(fp);
     }
 
     // adding relationships
@@ -72,18 +72,28 @@ hash_table_t* read_from_file(const char *file_path) {
         char node1_type;
         char node1_name[MAX_STR];
         int ret = get_next_name(fp, node1_name, &node1_type);
+        printf("ret: %d\n", ret);
         if (ret == EOF)
             break;
+        printf("%c, %s\n", node1_type, node1_name);
         graph_node_t* node1 = search_table(ht, node1_type, node1_name);
+        // get rid of the next space
+        char nothing = fgetc(fp);
+
         while (true) {
+            
             // get name/type of node2
             char node2_type;
             char node2_name[MAX_STR];
             int ret = get_next_name(fp, node2_name, &node2_type);
+            printf("ret: %d\n", ret);
             if (ret == EOF || ret == '\n')
                 break;
+            printf("%c, %s\n", node2_type, node2_name);
             graph_node_t* node2 = search_table(ht, node2_type, node2_name);
             add_neighbor(node1, node2);
+            // get rid of the next space
+            nothing = fgetc(fp);
         }
     }
 
@@ -109,6 +119,7 @@ int get_next_name(FILE* fp, char name[MAX_STR], char* c) {
         str_len++;
     }
     name[str_len] = '\0';
+
     return 0;
 }
 
